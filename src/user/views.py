0b3a062c7afe9de0fd3ecf7ab58b0c6d79e5a6e5
Omar <omaralhoori@gmail.com>
2020-02-26 02:43:33 +0300

    Add Register Form

--- src/user/views.py
+++ src/user/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
+
+def register(req):
+    if req.method == 'POST':
+        form = UserCreationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            messages.success(req, f'تهانينا {username} ,لقد تم التسجيل بنجاح ')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(req, 'user/register.html',{
+        'title': 'التسجيل',
+        'form': form,
+    })
