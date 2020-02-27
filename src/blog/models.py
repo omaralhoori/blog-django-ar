3a4717a1651eb8a37d7c8491d4a4d1618e50784e
Omar <omaralhoori@gmail.com>
2020-02-27 16:26:23 +0300

    Add new post page

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -12,6 +13,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        # return '/detail/{}'.format(self.pk)
+        return reverse('detail', args=[self.pk])
+    
     
     class Meta:
         ordering = ('-post_date',)
--- src/blog/urls.py
+++ src/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('detail/<int:post_id>/', views.post_detail, name='detail')
+    path('detail/<int:post_id>/', views.post_detail, name='detail'),
+    path('new_post/', views.PostCreateView.as_view(), name='new_post'),
 ]
