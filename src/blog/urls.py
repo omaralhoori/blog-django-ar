101a62d35baace56be04a526e545f897ae159d4b
Omar <omaralhoori@gmail.com>
2020-02-29 19:34:51 +0300

    fix some issues

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('detail/<int:post_id>/', views.post_detail, name='detail'),
+    path('detail/<slug:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
     path('new_post/', views.PostCreateView.as_view(), name='new_post'),
+    path('detail/<slug:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post,Comment
 from .forms import NewComment, PostCreateForm
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.views.generic import CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 def home(req):
@@ -56,3 +56,29 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(UserPassesTestMixin,LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'blog/post_update.html'
+    form_class = PostCreateForm
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else :
+            return False
+
+class PostDeleteView(UserPassesTestMixin,LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else :
+            return False
